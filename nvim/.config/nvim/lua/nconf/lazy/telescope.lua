63de6accfb6e95d59f045058c0ff33c354ddeb86
Antonio Gemesson <antonio.gemesson@ufpi.edu.br>
2026-07-16 23:52:07 -0300

    resolve erro no telescope

--- nvim/.config/nvim/lua/nconf/lazy/telescope.lua
+++ nvim/.config/nvim/lua/nconf/lazy/telescope.lua
@@ -5,7 +5,14 @@ return {
   module = "telescope",
 
   config = function()
-    require('telescope').setup({})
+    require('telescope').setup({
+      defaults = {
+        preview = {
+          treesitter = false,
+        },
+      },
+    })
+    
 
     local builtin = require('telescope.builtin')
 
