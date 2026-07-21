66f92278ec39fbf97707eed46cd934d35d7206db
Antonio Gemesson <antonio.gemesson@ufpi.edu.br>
2026-07-21 02:23:01 -0300

    Adiciona a configuracao do alacritty, uma opcao ao kitty

--- nvim/.config/nvim/lua/nconf/remap.lua
+++ nvim/.config/nvim/lua/nconf/remap.lua
@@ -9,3 +9,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.keymap.set("n", "<leader>cr", function()
+  -- Pega o tipo do arquivo, o nome com extensão (ex: main.cpp) e sem extensão (ex: main)
+  local filetype = vim.bo.filetype
+  local filename = vim.fn.expand("%")
+  local filename_no_ext = vim.fn.expand("%:r")
+  local cmd = ""
+
+  if filetype == "cpp" then
+    cmd = string.format("g++ %s -o %s && ./%s", filename, filename_no_ext, filename_no_ext)
+  elseif filetype == "go" then
+    cmd = string.format("go run %s", filename)
+  else
+    print("Linguagem não configurada para rodar!")
+    return
+  end
+
+  vim.cmd("TermExec cmd='" .. cmd .. "'")
+end, { desc = "Rodar código" })
