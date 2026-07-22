50e5fcbb0e36aa4d3063a050ccc798a221c237b3
Antonio Gemesson <antonio.gemesson@ufpi.edu.br>
2026-07-22 20:06:06 -0300

    Atualiza o script de instalação

--- install.sh
+++ install.sh
@@ -1,31 +1,43 @@
 #!/usr/bin/env bash
 
-set -euo pipefall
+set -euo pipefail
 
+echo "-----Instaling pacman needed packages-----"
 sudo pacman -Syu --needed \
     git go base-devel stow \
-    obs-studio pipewire pipewire-pulse wireplumber \
+    pipewire pipewire-pulse \
+    wireplumber alacritty \
     xdg-desktop-portal-gnome \
-    neovim firefox fastfetch kitty alacritty \
-    lf okular vlc steam loupe make 
+    neovim firefox fastfetch \
+    kitty lf okular vlc loupe \
+    make less papirus-icon-theme \
+    starship ttf-jetbrains-mono
 
-if ! comand -v yay &>/dev/null; then
-    git clone https://aur.archlinux.org/yay.git 
-    cd yay 
+if ! command -v yay &>/dev/null; then
+    "------Instaling AUR helper (yay)-----"
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
     makepkg -si
     cd ..
     rm -rf yay
 fi 
 
+echo "-----Instaling yay needed packages-----"
 yay -S --needed \
     niri \
     noctalia-shell \
-    android-studio \
-    arduino-ide-bin
 
-stow alacritty 
+echo "-----Setting up config files-----"
+stow alacritty
 stow fastfetch
-stow kitty 
-stow niri 
-stow nvim 
+stow kitty
+stow niri
+stow nvim
 
+echo "-----Setting up Bashrc-----"
+echo "fastfetch" >> $HOME/.bashrc
+echo "eval "$(starship init bash)"" >> $HOME/.bashrc
+
+starship preset catppuccin-powerline -o ~/.config/starship.toml
+
+echo "config finished"
